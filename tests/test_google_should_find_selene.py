2e05df0989d718a4eb75a7a431f1b90ba8ffb2f2
ekat-igorevna <ekat.igorevna@gmail.com>
2023-09-18 15:00:39 +0300

    Перенесла browser.open('https://google.com') в тесты.

--- tests/test_google_should_find_selene.py
+++ tests/test_google_should_find_selene.py
@@ -9,16 +9,13 @@ def browser_size():
     browser.config.window_height = 720
 
 
-@pytest.fixture
-def open_url():
+def test_search(browser_size):
     browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in'))
 
 
-def test_search(browser_size, open_url):
-     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in'))
-
-
-def test_search_no_result(browser_size, open_url):
+def test_search_no_result(browser_size):
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('fdfdfdfdfdh141cnjmmmmmm').press_enter()
     browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
